cebcfc3f9178ca863b137b632e6d55b307bebb97
DD <denis.denysenko404@gmal.com>
2026-05-09 01:35:07 +0300

    was added me with route.js and fixed some errors in the middleware.js

--- app/api/auth/me/route.js
+++ app/api/auth/me/route.js
@@ -1,2 +1,35 @@
-import {NextReposne} from 'next/server'
+import {NextResponse} from 'next/server'
 import {jwtVerify} from 'jose'
+import prisma from '@/app/lib/prisma'
+
+export async function GET(request){
+    const token = request.cookies.get('token')?.value
+
+    if(!token){
+        return NextResponse.json(
+            {message: 'Not authenticated'},
+            {status:401}
+        )
+    }
+    try{
+        const secret = new TextEncoder().encode(process.env.JWT_SECRET)
+        const {payload} = await jwtVerify(token, secret)
+
+        const user = await prisma.user.findUnique({
+            where: {id: payload.userId}
+        })
+        if(!user){
+            return NextResponse.json(
+                {status: 401}
+            )
+        }
+        return NextResponse.json({
+        user: {id: user.id, name: user.name, email: user.email}
+        })
+    } catch {
+        return NextResponse.json(
+            {message: 'Something went wrong'},
+            {status: 401}
+        )
+    }
+}
